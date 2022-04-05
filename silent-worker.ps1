d6c542741ecb19eb521d8112c699a4327c20861a
Max <71935156+MaXfAa@users.noreply.github.com>
2022-04-06 01:40:50 +0200

    Update silent-worker.ps1

--- silent-worker.ps1
+++ silent-worker.ps1
@@ -1,2 +1,2 @@
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft" -Name Dsh
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft" -Name "Dsh"
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Dsh" -Name "AllowNewsAndInterests" -Value "0" -PropertyType "DWord"
